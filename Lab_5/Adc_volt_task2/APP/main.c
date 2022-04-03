7a167dd13c350829b75986d5336bc3be9b8b6530
DESKTOP-8AQ5VUP\U3 <abdelrahman.yousry000@gmail.com>
2022-04-03 22:24:28 +0200

    updating in the logic of the code

--- Lab_5/Adc_volt_task2/APP/main.c
+++ Lab_5/Adc_volt_task2/APP/main.c
@@ -18,33 +18,54 @@ int main (void)
 	u32 AnalogValue=0;
 	u8 flag=0;
 	u8 buffer[20];
+	u8 switchbtn1=1;
+	u8 switchbtn2=1;
+	Lcd_vidGoTo(1,8);
+	Lcd_vidDiaplayChar(buffer[0]);
+	Lcd_vidDiaplayChar('.');
+	Lcd_vidDiaplayChar(buffer[1]);
+	Lcd_vidDiaplayChar(buffer[2]);
+	Lcd_vidDiaplayChar(buffer[3]);
+	Lcd_vidGoTo(1,14);
+	Lcd_vidDiaplayString("  ");
+	Lcd_vidGoTo(1,14);
+	Lcd_vidDiaplayString("v");
 	Port_vidInit();
 	Adc_vidInit();
 	Adc_vidControl(ADC_u8ENABLE);
 	Lcd_vidInit();
-	Lcd_vidDiaplayString((pu8)"Volt");
+	Lcd_vidDiaplayString((pu8)"Welcome to the");
+	Lcd_vidGoTo(1,0);
+	Lcd_vidDiaplayString((pu8)"Voltameter");
+	_delay_ms(3000);
+	Lcd_vidSendCommand(LCD_u8CLEAR_DISPAY);
+	Lcd_vidGoTo(0,0);
+	Lcd_vidDiaplayString((pu8)"Choose unit");
+	Lcd_vidGoTo(1,0);
+	Lcd_vidDiaplayString((pu8)"1-V         2-mV");
+
 	while (1)
 	{
-		u8 switchbtn=0;
+
 		Adc_vidStartConversion();
 		AdcResult=Adc_u16GetResult();
 		AnalogValue=(AdcResult* ((5000)/1024));
 		itoa(AnalogValue,buffer,10);
-		Dio_enuReadChannelId(DIO_PIN_24,&switchbtn);
-		if(switchbtn==0)
+		Dio_enuReadChannelId(DIO_PIN_24,&switchbtn1);
+		Dio_enuReadChannelId(DIO_PIN_25,&switchbtn2);
+		if(switchbtn1==0)
 		{
-			flag^=1;
-			_delay_ms(200);
+			flag=1;
+			Lcd_vidSendCommand(LCD_u8CLEAR_DISPAY);
 		}
-		if(flag==0)
+		else if(switchbtn2==0)
 		{
-			Lcd_vidGoTo(1,8);
-			Lcd_vidDiaplayString(buffer);
-			Lcd_vidGoTo(1,14);
-			Lcd_vidDiaplayString("mv");
+			flag=2;
+			Lcd_vidSendCommand(LCD_u8CLEAR_DISPAY);
 		}
-		else
+		if (flag == 1)
 		{
+			Lcd_vidDiaplayString((pu8)"Volt");
 			Lcd_vidGoTo(1,8);
 			Lcd_vidDiaplayChar(buffer[0]);
 			Lcd_vidDiaplayChar('.');
@@ -54,19 +75,25 @@ int main (void)
 			Lcd_vidGoTo(1,14);
 			Lcd_vidDiaplayString("  ");
 			Lcd_vidGoTo(1,14);
-			Lcd_vidDiaplayString("v");
+			Lcd_vidDiaplayString("v ");
+			Dio_enuReadChannelId(DIO_PIN_25,&switchbtn2);
 		}
 
-//		Lcd_vidGoTo(1,8);
-//		itoa(AnalogValue,buffer,10);
-//		Lcd_vidDiaplayString(buffer);
-////		Lcd_vidDisplayNumber(AnalogValue);
-//		Lcd_vidDiaplayChar('.');
-//		AnalogValue=(AdcResult* (u32)((5*num)/1024)*10)-(AdcResult-AnalogValue);
-////		Lcd_vidDisplayNumber(AnalogValue);
-//		itoa(AnalogValue,buffer,10);
-//		Lcd_vidDiaplayString(buffer);
+		else if(flag==2)
+		{
 
+			Lcd_vidDiaplayString((pu8)"Volt");
+			Lcd_vidGoTo(1,8);
+			Lcd_vidDiaplayString(buffer);
+			Lcd_vidGoTo(1,14);
+			Lcd_vidDiaplayString("mv");
+			Dio_enuReadChannelId(DIO_PIN_24,&switchbtn1);
+		}
+
+		else
+		{
+
+		}
 	}
 
 	return 0 ;
